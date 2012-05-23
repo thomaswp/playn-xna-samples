bfc59dfc1f68cfcd9bc1f5892624214bbe1e3d69
Michael Bayne <mdb@samskivert.com>
2012-05-23 13:31:48 -0700

    Use latest ikvm-maven-plugin, assets dir, etc.

--- hello/ios/Main.cs
+++ hello/ios/Main.cs
@@ -6,24 +6,21 @@ using playn.ios;
 using playn.core;
 using playn.sample.hello.core;
 
-namespace playn.sample.hello.ios
-{
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			IOSPlatform.register(app);
-		    PlayN.run(new HelloGame());
-    		return true;
-		}
-	}
+namespace playn.sample.hello.ios {
 
-	public class Application
-	{
-		static void Main (string[] args)
-		{
-			UIApplication.Main (args, null, "AppDelegate");
-		}
-	}
+  [Register ("AppDelegate")]
+  public partial class AppDelegate : UIApplicationDelegate {
+    public override bool FinishedLaunching (UIApplication app, NSDictionary options) {
+      var p = IOSPlatform.register(app);
+      p.assets().setPathPrefix("assets");
+      PlayN.run(new HelloGame());
+      return true;
+    }
+  }
+
+  public class Application {
+    static void Main (string[] args) {
+      UIApplication.Main (args, null, "AppDelegate");
+    }
+  }
 }
