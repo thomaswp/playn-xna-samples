9172425b50387d39f73b67df8644128504887b15
Michael Bayne <mdb@samskivert.com>
2013-07-19 15:52:38 -0700

    Cross-platform Flurry interface and example.

--- playn-flurry/core/src/main/java/playn/flurry/core/FlurryExample.java
+++ playn-flurry/core/src/main/java/playn/flurry/core/FlurryExample.java
@@ -1,23 +1,38 @@
 package playn.flurry.core;
 
+import playn.core.*;
 import static playn.core.PlayN.*;
-
-import playn.core.Game;
-import playn.core.Image;
-import playn.core.ImageLayer;
 
 public class FlurryExample extends Game.Default {
 
-  public FlurryExample() {
+  public final Flurry flurry;
+
+  public FlurryExample(Flurry flurry) {
     super(33); // call update every 33ms (30 times per second)
+    this.flurry = flurry;
   }
 
   @Override
   public void init() {
-    // create and add background image layer
     Image bgImage = assets().getImage("images/bg.png");
     ImageLayer bgLayer = graphics().createImageLayer(bgImage);
     graphics().rootLayer().add(bgLayer);
+
+    TextFormat fmt = new TextFormat().withWrapWidth(graphics().width()).withFont(
+      graphics().createFont("Helvetica", Font.Style.PLAIN, 32));
+    TextLayout layout = graphics().layoutText("Click anywhere to log flurry event.", fmt);
+    CanvasImage image = graphics().createImage(layout.width(), layout.height());
+    image.canvas().fillText(layout, 0, 0);
+    graphics().rootLayer().addAt(graphics().createImageLayer(image),
+                                 (graphics().width()-image.width())/2,
+                                 (graphics().height()-image.height())/2);
+
+    bgLayer.addListener(new Pointer.Adapter() {
+      public void onPointerStart(Pointer.Event event) {
+        flurry.logEvent("pointer_clicked", "time", (int)event.time(),
+                        "x", (int)event.x(), "y", (int)event.y());
+      }
+    });
   }
 
   @Override
@@ -26,6 +41,5 @@ public class FlurryExample extends Game.Default {
 
   @Override
   public void paint(float alpha) {
-    // the background automatically paints itself, so no need to do anything here!
   }
 }
